b66a7f8bc78111fa9803d38edfd2d1c78a5c838f
Nícolas Francisco Martins Chagas <125323611+Nickfmc07@users.noreply.github.com>
2024-02-14 17:48:47 -0300

    Update meuJs.js

--- meuJs.js
+++ meuJs.js
@@ -1,50 +1,59 @@
 const mainEl = document.querySelector('main');
 const navEl = document.querySelector('nav');
 const footerEl = document.querySelector('footer');
-const switchEl = document.querySelector('#switch');
-const btnTemasEl = document.querySelector('#alternar-tema'); 
-const bolaEl = document.querySelector('#bola');
-const h2 = document.querySelectorAll('h2');
-const luaEl = document.querySelector('#lua');
-const solEl = document.querySelector('#sol');
+const switches = document.querySelectorAll('.switch');
+const btnsTemas = document.querySelectorAll('.alternar-tema'); 
+const bolas = document.querySelectorAll('.bola');
+const luas = document.querySelectorAll('.luas');
+const sois = document.querySelectorAll('.sois');
 const imgsLaterais = document.querySelectorAll('.imgsLaterais')
 
-btnTemasEl.addEventListener('click', () => {
-    mainEl.classList.toggle('tema-escuro');
-    mainEl.classList.toggle('tema-claro');
-    footerEl.classList.toggle('tema-escuro');
-    footerEl.classList.toggle('tema-claro');
-    switchEl.classList.toggle('tema-escuro');
-    switchEl.classList.toggle('tema-claro');
-    h2.forEach (h2El => {
-        h2El.classList.toggle('tema-claro');
-        h2El.classList.toggle('tema-escuro');
+function alternarTemas (btnTemasEl) {
+    btnTemasEl.addEventListener('click', () => {
+        mainEl.classList.toggle('tema-escuro');
+        mainEl.classList.toggle('tema-claro');
+        footerEl.classList.toggle('tema-escuro');
+        footerEl.classList.toggle('tema-claro');
+        switches.forEach (switchEl => {
+            switchEl.classList.toggle('tema-claro');
+            switchEl.classList.toggle('tema-escuro');
+        });
+    
+        navEl.classList.toggle('icones-escuros');
+        navEl.classList.toggle('icones-claros');
+    
+        mainEl.classList.toggle('box-shadow-escuro');
+        footerEl.classList.toggle('box-shadow-escuro');
+        mainEl.classList.toggle('box-shadow-claro');
+        footerEl.classList.toggle('box-shadow-claro');
+        imgsLaterais.forEach (imgLateralEl => {
+            imgLateralEl.classList.toggle('box-shadow-claro2');
+            imgLateralEl.classList.toggle('box-shadow-escuro2');
+        });
+    
+        
+        bolas.forEach (bolaEl => {
+            bolaEl.classList.toggle('bola-checked');
+            bolaEl.classList.toggle('bola-unchecked');
+        });
+        luas.forEach (luaEl => {
+            luaEl.classList.toggle('checked');
+        });
+        sois.forEach (solEl => {
+            solEl.classList.toggle('checked');
+        });
     });
+}
 
-    navEl.classList.toggle('icones-escuros');
-    navEl.classList.toggle('icones-claros');
-
-    mainEl.classList.toggle('box-shadow-escuro');
-    footerEl.classList.toggle('box-shadow-escuro');
-    mainEl.classList.toggle('box-shadow-claro');
-    footerEl.classList.toggle('box-shadow-claro');
-    imgsLaterais.forEach (imgLateralEl => {
-        imgLateralEl.classList.toggle('box-shadow-claro2');
-        imgLateralEl.classList.toggle('box-shadow-escuro2');
-    });
-
-    bolaEl.classList.toggle('bola-checked');
-    bolaEl.classList.toggle('bola-unchecked');
-    luaEl.classList.toggle('checked');
-    solEl.classList.toggle('checked');
+btnsTemas.forEach (btnTemasEl => {
+    alternarTemas(btnTemasEl);
 });
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     mainEl.classList.add('tema-escuro');
     footerEl.classList.add('tema-escuro');
-    switchEl.classList.add('tema-escuro');
-    h2.forEach (h2El => {
-        h2El.classList.add('tema-escuro');
+    switches.forEach (switchEl => {
+        switchEl.classList.add('tema-escuro');
     });
 
     navEl.classList.add('icones-escuros');
@@ -55,14 +64,17 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
         imgLateralEl.classList.add('box-shadow-escuro2');
     });
 
-    bolaEl.classList.add('bola-checked');
-    luaEl.classList.add('checked');
+    bolas.forEach (bolaEl => {
+        bolaEl.classList.add('bola-checked');
+    });
+    luas.forEach (luaEl => {
+        luaEl.classList.add('checked');
+    });
 } else {
     mainEl.classList.add('tema-claro');
     footerEl.classList.add('tema-claro');
-    switchEl.classList.add('tema-claro');
-    h2.forEach (h2El => {
-        h2El.classList.add('tema-claro');
+    switches.forEach (switchEl => {
+        switchEl.classList.add('tema-claro');
     });
 
     navEl.classList.add('icones-claros');
@@ -74,6 +86,10 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
     });
 
 
-    bolaEl.classList.add('bola-unchecked');
-    solEl.classList.add('checked');
+    bolas.forEach (bolaEl => {
+        bolaEl.classList.add('bola-unchecked');
+    });
+    sois.forEach (solEl => {
+        solEl.classList.add('checked');
+    });
 }
